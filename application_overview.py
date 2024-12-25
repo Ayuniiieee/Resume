035ed9f52fa0a7df8a9a57672e338ce0c7f9c21b
Ayuniiieee <ayukhadijah02@gmail.com>
2024-12-25 03:54:18 +0000

    update application

--- application_overview.py
+++ application_overview.py
@@ -25,14 +25,12 @@ def fetch_applications(user_email):
             .from_('job_applications')
             .select('''
                 id,
-                job_id,
-                jobs:job_id (job_title, job_subject, parent_email),
-                user_id,
-                users:user_id (full_name),
+                job_id (job_title, job_subject, parent_email),
+                user_id (full_name),
                 resume_path,
                 status
             ''')
-            .eq('jobs.parent_email', user_email)
+            .eq('job_id.parent_email', user_email)  # Adjusted to use the correct foreign key reference
             .execute()
         )
 
